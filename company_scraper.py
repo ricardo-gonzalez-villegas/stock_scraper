ea68b8ff5bdbe83c0ab5986ee1447257459aa4f4
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2023-09-19 17:55:33 -0400

    "Reformatted scraper to scrape all data cels in table."

--- company_scraper.py
+++ company_scraper.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium import webdriver
 import time
 
+# companies are sorted by market cap
 URL = "https://stockanalysis.com/list/biggest-companies/"
 
 options = Options()
@@ -17,18 +18,20 @@ driver.get(URL)
 total_page_count = int(driver.find_element(By.XPATH, "//nav/div/span").text.split(" ")[3])
 page_count = 1
 
+data_cells = list()
 
-# rank - symbol - name
+
 def scrape_data() -> None:
     # wait for page to load
     time.sleep(2)
-    # scrapes stock symbols
-    symbols = driver.find_elements(By.CLASS_NAME, "sym")
-    for symbol in symbols:
-        print(symbol.text)
+    # scrapes data cell elements in table
+    data_cell_elements = driver.find_elements(By.TAG_NAME, "td")
+    # adds scraped data to list
+    for data_element in data_cell_elements:
+        data_cells.append(data_element.text)
 
 
-# click through all pages and scape data
+# click through all pages and scrape data
 while True:
     # checks if on last page and scrapes data before exiting
     if page_count == total_page_count:
@@ -37,10 +40,11 @@ while True:
     # scrapes page and increases page count
     try:
         scrape_data()
-        driver.find_element(By.XPATH, "//span[contains(text(),'Next')]").click()
         page_count = page_count + 1
+        driver.find_element(By.XPATH, "//span[contains(text(),'Next')]").click()
     # close pop up if it displays
-    except:
+    except Exception as e:
+        print("pop up")
         WebDriverWait(driver, 2).until(
             ec.presence_of_element_located((By.CSS_SELECTOR, "[aria-label='Close']")))
         driver.find_element(By.CSS_SELECTOR, "[aria-label='Close']").click()

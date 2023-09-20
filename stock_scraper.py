7f1b9d4d809e777240e49c3b5258fdc34064b406
Ricardo Gonzalez <rikarudogonzarezu@gmail.com>
2023-09-20 17:16:55 -0400

    Refactored code to scrape table rows to be faster & created function to run scraping

--- stock_scraper.py
+++ stock_scraper.py
@@ -16,35 +16,34 @@ driver = webdriver.Chrome(options=options)
 driver.get(URL)
 
 total_page_count = int(driver.find_element(By.XPATH, "//nav/div/span").text.split(" ")[3])
-page_count = 1
-
-data_cells = list()
 
 
-def scrape_data() -> None:
+# scrape table rows
+def scrape_data(table_rows: list) -> None:
     # wait for page to load
     time.sleep(2)
-    # scrapes data cell elements in table
-    data_cell_elements = driver.find_elements(By.TAG_NAME, "td")
+    # scrapes table row elements in table
+    table_row_elements = driver.find_elements(By.TAG_NAME, "tr")
     # adds scraped data to list
-    for data_element in data_cell_elements:
-        data_cells.append(data_element.text)
+    for table_row in table_row_elements:
+        table_rows.append(table_row.text)
 
 
 # click through all pages and scrape data
-while True:
-    # checks if on last page and scrapes data before exiting
-    if page_count == total_page_count:
-        scrape_data()
-        break
-    # scrapes page and increases page count
-    try:
-        scrape_data()
-        page_count = page_count + 1
-        driver.find_element(By.XPATH, "//span[contains(text(),'Next')]").click()
-    # close pop up if it displays
-    except Exception as e:
-        print("pop up")
-        WebDriverWait(driver, 2).until(
-            ec.presence_of_element_located((By.CSS_SELECTOR, "[aria-label='Close']")))
-        driver.find_element(By.CSS_SELECTOR, "[aria-label='Close']").click()
+def run_stock_scraper() -> list:
+    table_rows = list()
+    page_count = 0
+
+    while page_count != total_page_count:
+        # scrapes page and increases page count
+        try:
+            scrape_data(table_rows)
+            driver.find_element(By.XPATH, "//span[contains(text(),'Next')]").click()
+            page_count = page_count + 1
+        # close pop up if it displays
+        except Exception as e:
+            WebDriverWait(driver, 2).until(
+                ec.presence_of_element_located((By.CSS_SELECTOR, "[aria-label='Close']")))
+            driver.find_element(By.CSS_SELECTOR, "[aria-label='Close']").click()
+
+    return table_rows
